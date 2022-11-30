0948e979d31a5d933d444c077bb28c8f5e85232b
cwp21 <119521507+cwp21@users.noreply.github.com>
2022-11-30 11:51:45 -0500

    Update calculate_coordinates.py

--- calculate_coordinates.py
+++ calculate_coordinates.py
@@ -20,7 +20,7 @@ def generate_theta_random(n: int):
     return theta_array
 
 
-def calc_xy(radius, theta: list, x0, y0) -> tuple:
+def calc_xy(radius, theta: list, x0, y0):
     x = radius * np.cos(theta)
     y = radius * np.sin(theta)
     return x+x0, y+y0
