f8fdbc00dda0e1e278dbe2e9b3409346581a42fe
stalbot8 <65781297+stalbot8@users.noreply.github.com>
2022-12-01 13:38:00 -0500

    Updated input function with checks and prints

--- get_user_input.py
+++ get_user_input.py
@@ -1,12 +1,32 @@
 ## Function to get user input for required values
 
 def get_circle_inputs():
-    x0 = input("Enter the x-coordinate of the circle: ")
-    y0 = input("Enter the y-coordinate of the circle: ")
-    R = input("Enter the radius of the circle: ")
-    N = input("Enter the number of perimeter points on the circle: ")
-    spacing = input("Enter \'1\' for equally spaced points or \'2\' for randomly spaced points: ")
+    again = True
+    while again == True:
+        # Obtaining circle inputs
+        x0 = float(input("Enter the x-coordinate of the circle: "))
+        y0 = float(input("Enter the y-coordinate of the circle: "))
+        R = float(input("Enter the radius of the circle: "))
+        N = int(input("Enter the number of perimeter points on the circle: "))
+        spacing = int(input("Enter \'1\' for equally spaced points or \'2\' for randomly spaced points: "))
+
+        # Printing inputs
+        print("\n======USER INPUTS======")
+        print("x-coordinate: {}".format(x0))
+        print("y-coordinate: {}".format(y0))
+        print("radius: {}".format(R))
+        print("number of perimeter points: {}".format(N))
+        print("spacing selection: {}\n".format(spacing))
+
+        # Chacking statisfaction with inputs
+        answer1 = input("Are you satisfied with these values? (y/n): ")
+        if answer1.lower() == 'y':
+            again = False
+        else:
+            answer2 = input("Would you like to repeat the inputs? (y/n): ")
+            if answer2.lower() == 'n':
+                again = False
     return x0, y0, R, N, spacing
 
 if __name__ == '__main__':
-    get_circle_inputs()
+    x0, y0, R, N, spacing = get_circle_inputs()
--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from reader import feed
 
 def main():
 
-tic_circ = time.perf_counter()
-"Insert Circ Function Here
-toc_circ = time.perf_counter()
-print(f"Circumference approximated in {toc_circ-tic_circ:0.4f} seconds")
+    tic_circ = time.perf_counter()
+    #==========Insert Circ Function Here=========
+    toc_circ = time.perf_counter()
+    print(f"Circumference approximated in {toc_circ-tic_circ:0.4f} seconds")
 
-""Repeatable for actual calculation of 2pi*r and pi*R^2
+    #==========Repeatable for actual calculation of 2pi*r and pi*R^2==========
 
 
 
-tic_area = time.perf_counter()
-"Insert Area Function Here
-toc_area = time.perf_counter()
-print(f"Area approximated in {toc_circ-tic_circ:0.4f} seconds")
+    tic_area = time.perf_counter()
+    #==========Insert Area Function Here==========
+    toc_area = time.perf_counter()
+    print(f"Area approximated in {toc_circ-tic_circ:0.4f} seconds")
 
