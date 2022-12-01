729a1e414751ec07747bed997e115356e7e03ca7
gracefalanga <118743068+gracefalanga@users.noreply.github.com>
2022-12-01 10:10:32 -0500

    finish area estimate function

--- estimate_area.py
+++ estimate_area.py
@@ -2,13 +2,16 @@ import numpy as np
 from calculate_coordinates import get_theta_array
 from estimate_circumference import calc_distance
 
-def estimate_area():
+def estimate_area(r,n,choice):
     #area of isocelese traingle is b*h/2 (h = pythag using r and distance between points)
     theta_ar = get_theta_array(n, choice)
+    thetas = list(theta_ar)
     thetas = np.sort(thetas)
-    estimated_circ = 0
+    estimated_area = 0
     for i in range(len(thetas) - 1):
-        estimated_circ += calc_distance(r, thetas[i], thetas[i + 1])
+        b = calc_distance(r, thetas[i], thetas[i + 1])
+        h = np.sqrt(r**2 - (b/2)**2)
+        estimated_area += b*h/2
 
     return estimated_area
 
@@ -16,7 +19,12 @@ if __name__ == '__main__':
     # inputs
     x0 = 0
     y0 = 0
-    n = 40
+    n = 1000
     r = 1
     choice = 2  # 1=uniform, 2=random
+
+    area_est = estimate_area(r,n,choice)
+
+    print(f'Estimated Area using {n} points: {estimate_area(r,n,choice)}')
+    print(f'True Area: {np.pi*r**2}')
 
