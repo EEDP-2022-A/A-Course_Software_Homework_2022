430734958af117eb4bc288a8509cff12f52216f7
sbalaji1 <reachsurya21@gmail.com>
2022-12-01 10:55:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ tic_circ = time.perf_counter()
 toc_circ = time.perf_counter()
 print(f"Circumference approximated in {toc_circ-tic_circ:0.4f} seconds")
 
+""Repeatable for actual calculation of 2pi*r and pi*R^2
+
+
 
 tic_area = time.perf_counter()
 "Insert Area Function Here
