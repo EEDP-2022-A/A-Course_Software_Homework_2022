dd095193e49e95661a78812b03aa635bb32e3690
sbalaji1 <reachsurya21@gmail.com>
2022-12-01 10:53:15 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+from reader import feed
+
+
+def main():
+
+tic_circ = time.perf_counter()
+"Insert Circ Function Here
+toc_circ = time.perf_counter()
+print(f"Circumference approximated in {toc_circ-tic_circ:0.4f} seconds")
+
+
+tic_area = time.perf_counter()
+"Insert Area Function Here
+toc_area = time.perf_counter()
+print(f"Area approximated in {toc_circ-tic_circ:0.4f} seconds")
