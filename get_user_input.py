61fcd61c44e63786122f3d8a9c0250c9a9b20e53
stalbot8 <65781297+stalbot8@users.noreply.github.com>
2022-12-05 20:32:08 -0500

    added error messages to user inputs

--- get_user_input.py
+++ get_user_input.py
@@ -1,14 +1,49 @@
 ## Function to get user input for required values
 
 def get_circle_inputs():
-    again = True
-    while again == True:
+    while True:
         # Obtaining circle inputs
-        x0 = float(input("Enter the x-coordinate of the circle: "))
-        y0 = float(input("Enter the y-coordinate of the circle: "))
-        R = float(input("Enter the radius of the circle: "))
-        N = int(input("Enter the number of perimeter points on the circle: "))
-        spacing = int(input("Enter \'1\' for equally spaced points or \'2\' for randomly spaced points: "))
+        while True:
+            try:
+                x0 = float(input("Enter the x-coordinate of the circle: "))
+            except ValueError:
+                print("USER ERROR: Please enter a number.")
+                continue
+            break
+        
+        while True:
+            try:
+                y0 = float(input("Enter the y-coordinate of the circle: "))
+            except ValueError:
+                print("USER ERROR: Please enter a number.")
+                continue
+            break
+        
+        while True:
+            try:
+                R = float(input("Enter the radius of the circle: "))
+            except ValueError:
+                print("USER ERROR: Please enter a number.")
+                continue
+            break
+        
+        while True:
+            try:
+                N = int(input("Enter the number of perimeter points on the circle: "))
+            except ValueError:
+                print("USER ERROR: Please enter an integer greater than 2")
+            break
+
+        while True:
+            try:
+                spacing = int(input("Enter \'1\' for equally spaced points or \'2\' for randomly spaced points: "))
+                if spacing != 1 or spacing != 2:
+                    raise Exception("USER ERROR: Please enter a value of '1' or '2'.")
+                    
+            except ValueError:
+                print("USER ERROR: Please enter a value of '1' or '2'.")
+                continue
+            break
 
         # Printing inputs
         print("\n======USER INPUTS======")
@@ -18,14 +53,14 @@ def get_circle_inputs():
         print("number of perimeter points: {}".format(N))
         print("spacing selection: {}\n".format(spacing))
 
-        # Chacking statisfaction with inputs
+        # Checking statisfaction with inputs
         answer1 = input("Are you satisfied with these values? (y/n): ")
         if answer1.lower() == 'y':
-            again = False
+            break
         else:
             answer2 = input("Would you like to repeat the inputs? (y/n): ")
             if answer2.lower() == 'n':
-                again = False
+                break
     return x0, y0, R, N, spacing
 
 if __name__ == '__main__':
